ac3a5d29cbe27e2cc479633796bef46fcee517a9
mv <mv@outsi.de>
2022-05-03 20:15:00 +0300

    feat: add 5

---
+++ 4/main.go
@@ -0,0 +1,64 @@
+package main
+
+/*Реализовать постоянную запись данных в канал (главный поток).
+Реализовать набор из N воркеров, которые читают произвольные данные из канала
+и выводят в stdout. Необходима возможность выбора количества воркеров при старте.
+Программа должна завершаться по нажатию Ctrl+C.
+Выбрать и обосновать способ завершения работы всех воркеров.*/
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+)
+
+func main() {
+	//get count (int) workers from args
+	countWorkers, err := strconv.Atoi((os.Args[1])) 
+	if err != nil {
+		panic(err)
+	}
+
+	workChannel := make(chan string, countWorkers) //channel for write and read data
+	exitSignal := make(chan os.Signal, 1) //channel for catching signal
+	exitWorker := make(chan bool, countWorkers) //channel 
+	signal.Notify(exitSignal, syscall.SIGINT, syscall.SIGTERM)
+
+	var wg sync.WaitGroup //wait all goroutines
+
+	// start goroutines, which reading
+	for i := 0; i < countWorkers; i++ {
+		wg.Add(1)
+		go func () {
+			defer wg.Done()
+			for {
+				select {
+				case data := <-workChannel:
+					fmt.Fprintln(os.Stdout, data) // Print "data"
+				case <- exitWorker:
+					fmt.Println("Worker finished")
+					return
+				}
+			}
+		} ()
+	}
+
+	for {	
+		select {
+		case workChannel <- "data": //writing "data" in channel 
+		case <-exitSignal: //get signal (ctrl + c)
+			for i := 0; i < countWorkers; i++ { 
+				exitWorker <- true //post all goroutines to end
+			}
+			wg.Wait() //wait all goroutines
+			close(workChannel) //close channels
+			close(exitSignal)
+			close(exitWorker)
+			fmt.Println("Finish")
+			return 
+		}
+	}
+}

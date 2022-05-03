37bdd08c18b7ab39f2505000ea74dd8df765af3a
mv <mv@outsi.de>
2022-05-03 15:52:21 +0300

    feat: add 2

--- 2/main.go
+++ 2/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+/*Написать программу, которая конкурентно рассчитает значение квадратов
+чисел взятых из массива (2,4,6,8,10) и выведет их квадраты в stdout.*/
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"runtime"
+	"sync"
+)
 
 func main() {
-	fmt.Println("2")
+
+	runtime.GOMAXPROCS(2) //set count procs
+
+	var wg sync.WaitGroup 
+
+	arr := [5]int{ 2, 4, 6, 8, 10 }
+
+	for _, val := range arr {
+		wg.Add(1)
+		go func (val int) {
+			defer wg.Done()
+			fmt.Fprintln(os.Stdout ,math.Pow(float64(val), 2)) //print in random order
+		} (val)
+	}
+
+	wg.Wait() //wait all goroutines
 }

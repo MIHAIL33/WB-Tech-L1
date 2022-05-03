2f11e579a5d21416b7b1235ea903e7612cf09bc6
mv <mv@outsi.de>
2022-05-04 02:10:34 +0300

    feat: add 8

---
+++ 8/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*Дана переменная int64. Разработать программу которая устанавливает i-й бит в 1 или 0.*/
+
+
+func main() {
+	//read number from args
+	number, err := strconv.Atoi((os.Args[1])) 
+	if err != nil {
+		panic(err)
+	}
+
+	//read i-th bit from args
+	i, err := strconv.Atoi((os.Args[2])) 
+	if err != nil {
+		panic(err)
+	}
+
+	//int to binary string
+	val := strconv.FormatInt(int64(number), 2) 
+	fmt.Println(val)
+	bits := strings.Split(val, "")
+
+	//replace
+	if bits[len(bits) - i] == "0" {
+		bits[len(bits) - i] = "1"
+	} else {
+		bits[len(bits) - i] = "0"
+	}
+
+	val = strings.Join(bits, "")
+	fmt.Println(val)
+	
+	//binary string to int
+	res, _ := strconv.ParseInt(val, 2, 64)
+	fmt.Println(res)
+}

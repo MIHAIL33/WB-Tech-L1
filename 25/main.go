48069d86579cc6c17380809feb0a9bdd6f50f377
mv <mv@outsi.de>
2022-05-06 18:12:23 +0300

    fix: 25

--- 25/main.go
+++ 25/main.go
@@ -8,11 +8,19 @@ import (
 )
 
 //wait "duration" seconds and print
-func Sleep(duration int) {
+func Sleep_After(duration int) {
 	fmt.Println(<- time.After(time.Duration(duration) * time.Second))
-} 
+}
+
+func Sleep_Tick(duration int) {
+	fmt.Println(<- time.Tick(time.Duration(duration) * time.Second))
+}
 
 func main() {
 	fmt.Println(time.Now())
-	Sleep(5)
+	Sleep_After(2)
+	Sleep_Tick(2)
+
+	ticker := time.NewTicker(2 * time.Second)
+	fmt.Println(<- ticker.C)
 }

823c48d394a7fbc7fe3e0bae9f5127a446b9f222
mv <mv@outsi.de>
2022-05-06 15:59:38 +0300

    feat: add 25

--- 25/main.go
+++ 25/main.go
@@ -3,9 +3,16 @@ package main
 /*Реализовать собственную функцию sleep.*/
 
 import (
-
+	"fmt"
+	"time"
 )
 
-func main() {
+//wait "duration" seconds and print
+func Sleep(duration int) {
+	fmt.Println(<- time.After(time.Duration(duration) * time.Second))
+} 
 
+func main() {
+	fmt.Println(time.Now())
+	Sleep(5)
 }
